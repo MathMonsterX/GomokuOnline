0408ad51ffb5eb229f8100fc27a99aed9b0d3356
Chris Boe <cboe14@yahoo.com>
2015-04-04 16:56:01 -0700

    Completed-ish admin model. Connects to server, gets list, sends authentication, opens menus

--- GomokuOnline/src/gomokuonline/AdminModel.java
+++ GomokuOnline/src/gomokuonline/AdminModel.java
@@ -1,13 +1,20 @@
 
 package gomokuonline;
 
+import java.io.*;
+import static java.lang.Thread.sleep;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * This model is meant to dictate what views appear and disappear
  * after certain buttons are clicked. Each method is called by the
  * appropriate controllers and views
  * @author Chris
  */
-public class AdminModel {
+public class AdminModel implements Runnable{
     ChooseAIController AIController;
     GameController gameController;
     LogInController logInController;
@@ -16,7 +23,117 @@ public class AdminModel {
     RegisterController registerController;
     StatController statController;
     
+    private int port = 54321;
+    private Socket clientSock;
+    private InputStream is;
+    private OutputStream os;
+    private BufferedWriter socketOut;
+    private BufferedReader socketIn;
     
+    private Thread worker;
+    
+    //opens default local address
+    public AdminModel(){
+        try{
+            System.out.println("Attempting to connect socket");
+            clientSock = new Socket( InetAddress.getLocalHost().getHostAddress(), port ) ;
+            System.out.println("Local Socket connection successful!");
+            is = clientSock.getInputStream() ;
+            os = clientSock.getOutputStream() ;
+            socketOut = new BufferedWriter(new OutputStreamWriter(os));
+            socketIn = new BufferedReader( new InputStreamReader(is) ) ;
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(AdminModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        }
+    }
+    
+    //connects to specified address
+    public AdminModel(String address, int port){
+        try{
+            clientSock = new Socket(address, port);
+            System.out.println("Connected: "+clientSock.isConnected());
+            is = clientSock.getInputStream();
+            os = clientSock.getOutputStream() ;
+            socketOut = new BufferedWriter(new OutputStreamWriter(os));
+            socketIn = new BufferedReader( new InputStreamReader(is));
+        }catch(IOException ex){
+            Logger.getLogger(AdminModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        }
+    }
+    
+    @Override
+    public void run() {
+        try{
+            while(true){
+                String message = "";
+                char msgChar = (char)socketIn.read();
+                message += msgChar;
+                
+                while(socketIn.ready()){
+                    msgChar = (char)socketIn.read();
+                    message += msgChar;
+                }
+                String[] input = message.split("//s+");
+                if(input[0].equals("AUTHENTICATION")){
+                    if(input[1].equals("success")){
+                        System.out.println("login successful");
+                        openMainMenu();
+                    }
+                    else{
+                        System.out.println("login failed");
+                    }
+                }else if(input[0].equals("ONLINE_USERS")){
+                    System.out.println("Receiving list of players...");
+                    postList(input);
+                }
+            }
+        }catch(IOException ex){
+            Logger.getLogger(AdminModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Failed in receiving message");
+        }
+    }
+    public void beginListening(){
+        worker = new Thread(this);
+        worker.start();
+    }
+    public void sendUserLogin(String user, String pass){
+        String message = "LOGIN" +  user + " " + pass;
+        try{
+            socketOut.write(message);
+            socketOut.flush();
+        }catch(IOException ex){
+            Logger.getLogger(AdminModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error in writing to socket");
+        }
+    }
+    
+    //get list of online players from server
+    public void getLoggedInList(){
+        String message = "GET_USERS";
+        try{
+            socketOut.write(message);
+            socketOut.flush();
+        }catch (IOException ex){
+            Logger.getLogger(AdminModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error in sending list request");
+        }
+    }
+    public void postList(String[] input){
+        //onlineMenuController.postList(input);
+    }
+    public void close(){
+        try{
+            this.clientSock.close();
+        }catch(IOException ex){
+            Logger.getLogger(AdminModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error in closing socket");
+
+        }
+    }
     public void openOnlineMenu(){
         if(onlineMenuController==null){
             onlineMenuController = new OnlineMenuController();
@@ -44,4 +161,5 @@ public class AdminModel {
         else
             mainMenuController.openView();
     }
+
 }
