a708647b3e1062f954187670560e1ec11276fcb2
Chris Boe <cboe14@yahoo.com>
2015-04-04 14:03:23 -0700

    Created admin model

---
+++ GomokuOnline/src/gomokuonline/AdminModel.java
@@ -0,0 +1,19 @@
+
+package gomokuonline;
+
+/**
+ * This model is meant to dictate what views appear and disappear
+ * after certain buttons are clicked. Each method is called by the
+ * appropriate controllers and views
+ * @author Chris
+ */
+public class AdminModel {
+    ChooseAIController AIController;
+    GameController gameController;
+    LogInController logInController;
+    MainMenuController mainMenuController;
+    OnlineMenuController onlineMenuController;
+    RegisterController registerController;
+    StatController statController;
+    
+}
