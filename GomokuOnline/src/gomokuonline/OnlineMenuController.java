d380777ecdae01e3cc1ef21a6e43ae1a070a9549
Chris Boe <cboe14@yahoo.com>
2015-04-04 17:16:23 -0700

    added timed request

--- GomokuOnline/src/gomokuonline/OnlineMenuController.java
+++ GomokuOnline/src/gomokuonline/OnlineMenuController.java
@@ -1,5 +1,9 @@
 package gomokuonline;
 
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author clarissapendleton
@@ -10,6 +14,7 @@ public class OnlineMenuController {
     
     public void createView(){
         view = new OnlineMenuView();
+        model = new AdminModel();
         model.createOnlineMenuFrame(view);
         view.makeVisible();
     }
@@ -18,5 +23,17 @@ public class OnlineMenuController {
     }
     public void postList(String[] players){
         view.post(players);
+    }
+    public void timedRequestList(){
+        while(true){
+            try {
+                sleep(15000);
+                model.getLoggedInList();
+                timedRequestList();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(OnlineMenuController.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Timer was interrupted");
+            }
+        }
     }
 }
