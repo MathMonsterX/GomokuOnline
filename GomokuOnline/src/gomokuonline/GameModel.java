a511a2adb5d166da959939da430f99b7b537be0a
pendleca <pendleca@plu.edu>
2015-04-22 16:58:37 -0700

    Game Model update

--- GomokuOnline/src/gomokuonline/GameModel.java
+++ GomokuOnline/src/gomokuonline/GameModel.java
@@ -1,9 +1,28 @@
 package gomokuonline;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+
 /**
  *
  * @author clarissapendleton
  */
-public class GameModel {
+public class GameModel implements Runnable{
+    private GameController controller ;
+    private Thread listenWorker;
+    private BufferedReader dataIn;
+    private DataOutputStream dataOut;
     
+    GameModel(){
+        
+    }
+    
+    GameModel(String ip){
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    }
 }
