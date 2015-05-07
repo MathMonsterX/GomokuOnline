148642e059dd6cb87ca38da487c520bcd8276764
pendleca <pendleca@plu.edu>
2015-05-07 14:37:09 -0700

    Merged things together

--- GomokuOnline/src/gomokuonline/GameController.java
+++ GomokuOnline/src/gomokuonline/GameController.java
@@ -1,55 +1,4 @@
-<<<<<<< Updated upstream
-package gomokuonline;
 
-import javax.swing.JFrame;
-
-/**
- *
- * @author clarissapendleton
- */
-public class GameController {
-    private GameModel model;
-    private GameView view;
-    private JFrame gameViewFrame;
-    
-    public void setModel(GameModel model){
-        this.model=model;
-    }
-    
-    public void createView(){
-        this.view = new GameView();
-        gameViewFrame = new JFrame();
-        //gameViewFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameViewFrame.setContentPane(view);
-        gameViewFrame.setVisible(true);
-        view.setController(this);
-        view.setFrame(gameViewFrame);
-        view.setupBoard(model.getSize(), model.getSize());
-        gameViewFrame.pack();
-
-    }
-    
-    public void setEndMoveEnabled(boolean enabled){
-        this.view.setbtnEndMOveEnabled(enabled);
-    }
-    
-    public void updateBoard(int row, int column){
-        view.updateBoard(row, column);
-    }
-    
-    public void endGame(int row, int column){
-        view.endGame(row, column);
-    }
-    
-    public void makeMove(int row, int col){
-        model.makeMove(row, col);
-    }
-    
-    public char getPlayerChar(){
-        return this.model.getPlayerChar();
-    }
-}
-=======
 package gomokuonline;
 
 import javax.swing.JFrame;
@@ -95,5 +44,11 @@ public class GameController {
     public void makeMoveToAI(int row, int col){
         model.AIGamePlay(row, col, 'o');
     }
+    public void setEndMoveEnabled(boolean enabled){
+        this.view.setbtnEndMOveEnabled(enabled);
+    }
+     public char getPlayerChar(){
+        return this.model.getPlayerChar();
+    }
 }
->>>>>>> Stashed changes
+
