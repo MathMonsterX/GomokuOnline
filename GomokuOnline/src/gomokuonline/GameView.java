011337d8ad06e528a880007e76ce5ab474057333
tkreamer <kreametj@plu.edu>
2015-05-06 19:51:49 -0700

    updated view to contain board

--- GomokuOnline/src/gomokuonline/GameView.java
+++ GomokuOnline/src/gomokuonline/GameView.java
@@ -1,6 +1,8 @@
 
 package gomokuonline;
 
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -10,6 +12,7 @@ import javax.swing.JFrame;
 public class GameView extends javax.swing.JPanel {
     private GameController controller;
     private JFrame gameViewFrame;
+    private GameButton[][] buttonGrid;
     /**
      * Creates new form GameView
      */
@@ -18,6 +21,25 @@ public class GameView extends javax.swing.JPanel {
         lblPlayer.setText("");
         lblPlayerMove.setVisible(false);
         lblOpponentMove.setVisible(false);
+    }
+    
+    
+    
+    /**
+     * Instantiates a width X length board of buttons.
+     * @param width width of the board
+     * @param length length of the board
+     */
+     public void setupBoard(int width, int length){
+        this.buttonGrid = new GameButton[width][length];
+        gameBoardPanel.setLayout(new GridLayout(width, length));
+        for(int y = 0; y<length; y++){
+                for(int x = 0; x <width; x++){
+                        this.buttonGrid[x][y] = new GameButton(x,y," ");
+                        gameBoardPanel.add(buttonGrid[x][y]);
+
+                }
+        }
     }
 
     /**
@@ -33,8 +55,14 @@ public class GameView extends javax.swing.JPanel {
         lblPlayerMove = new javax.swing.JLabel();
         lblOpponentMove = new javax.swing.JLabel();
         lblPlayer = new javax.swing.JLabel();
+        gameBoardPanel = new javax.swing.JPanel();
 
         btnEndMove.setText("End Move");
+        btnEndMove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEndMoveActionPerformed(evt);
+            }
+        });
 
         lblPlayerMove.setText("Your Name- Your Move! ");
 
@@ -42,32 +70,51 @@ public class GameView extends javax.swing.JPanel {
 
         lblPlayer.setText("Player");
 
+        javax.swing.GroupLayout gameBoardPanelLayout = new javax.swing.GroupLayout(gameBoardPanel);
+        gameBoardPanel.setLayout(gameBoardPanelLayout);
+        gameBoardPanelLayout.setHorizontalGroup(
+            gameBoardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 197, Short.MAX_VALUE)
+        );
+        gameBoardPanelLayout.setVerticalGroup(
+            gameBoardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 197, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblOpponentMove)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(32, 32, 32)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblOpponentMove)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblPlayerMove)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
+                                .addComponent(btnEndMove))))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblPlayerMove)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
-                        .addComponent(btnEndMove)))
+                        .addGap(23, 23, 23)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(gameBoardPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(lblPlayer))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(lblPlayer)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(lblPlayer)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 221, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(gameBoardPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(lblOpponentMove)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -77,9 +124,24 @@ public class GameView extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnEndMoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEndMoveActionPerformed
+        int row, col = 0;
+        for(int i = 0; i < buttonGrid.length; i++){
+            for(int j = 0; j < buttonGrid.length; j++){
+                row = i;
+                col = j;
+                
+                
+                        
+                
+            }
+        }
+    }//GEN-LAST:event_btnEndMoveActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnEndMove;
+    private javax.swing.JPanel gameBoardPanel;
     private javax.swing.JLabel lblOpponentMove;
     private javax.swing.JLabel lblPlayer;
     private javax.swing.JLabel lblPlayerMove;
@@ -100,6 +162,7 @@ public void updateLblOpponentMove(String message){
     
 }
 private void btnEndMove_Click(java.awt.event.ActionEvent evt){
+    
     
 }
 
